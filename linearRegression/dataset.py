db687bbb6b3d126de81731f18e827837be18cc93
SShowbiz <hwkim408@gmail.com>
2021-03-11 20:54:46 +0900

    fix: remove unnecessary print

--- linearRegression/dataset.py
+++ linearRegression/dataset.py
@@ -24,7 +24,7 @@ def makeDataset(parameterList, numData):
 class TrainDataset(Dataset):
     def __init__(self):
         (xData, yData) = makeDataset([1, 7, 8, 10], 100)
-        print(xData, yData)
+        # print(xData, yData)
         self.x = xData
         self.y = yData
 
