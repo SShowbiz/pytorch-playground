8623558dbb88bf287e32bd599bb3041e869eeefb
SShowbiz <hwkim408@gmail.com>
2021-03-12 21:24:09 +0900

    feat: add accuracy analyzing

--- logisticRegression/train.py
+++ logisticRegression/train.py
@@ -10,25 +10,34 @@ import matplotlib.pyplot as plt
 
 def train():
     numEpoch = 100
+    batchSize = 4
 
     dataset = TrainDataset()
-    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=batchSize, shuffle=True)
     numBatch = len(dataloader)
 
     LogisticRegressionModel = LogisticRegression(3)
     optimizer = torch.optim.SGD(LogisticRegressionModel.parameters(), lr=1e-5)
 
     loss = nn.BCELoss()
+    correct_number = 0.0
     for epoch in range(numEpoch + 1):
         for batch, data in enumerate(dataloader):
             x, y = data
-            batchLoss = loss(LogisticRegressionModel(x), y)
+            hypothesis = LogisticRegressionModel(x)
+            batchLoss = loss(hypothesis, y)
 
             optimizer.zero_grad()
             batchLoss.backward()
             optimizer.step()
-            print('EPOCH: {}/{} BATCH: {}/{} BCEloss: {:.6f}'.format(epoch,
-                                                                     numEpoch, batch, numBatch, batchLoss.item()))
+
+            prediction = hypothesis >= torch.FloatTensor([0.5])
+            correct_prediction = prediction.float() == y
+            correct_number += correct_prediction.sum()
+            accuracy = correct_number / \
+                ((epoch+1)*batchSize*numBatch + (batch+1)*batchSize) * 100
+            print('EPOCH: {}/{} BATCH: {}/{} BCEloss: {:.6f} ACCURACY: {}%'.format(epoch,
+                                                                                   numEpoch, batch, numBatch, batchLoss.item(), accuracy))
     for name, param in LogisticRegressionModel.named_parameters():
         if param.requires_grad:
             print(name, param.data)
