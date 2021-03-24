0d45f72b4d944fa6fed5bedcb9be94a3a908ddc8
SShowbiz <hwkim408@gmail.com>
2021-03-24 10:27:40 +0900

    feat: change Softmax model to LogSoftmax model

--- SoftmaxRegression/model.py
+++ SoftmaxRegression/model.py
@@ -6,7 +6,7 @@ class SoftmaxRegression(nn.Module):
     def __init__(self, n, m):  # n 개의 인자를 가진 다중 선형 회귀의 m중 분류를 구현
         super().__init__()
         self.linearRegressionModel = nn.Linear(n, m, bias=True)
-        self.softmaxRegressionModel = nn.Softmax()
+        self.softmaxRegressionModel = nn.LogSoftmax()
 
     def forward(self, x):  # forward propagation
         return self.softmaxRegressionModel(self.linearRegressionModel(x))
